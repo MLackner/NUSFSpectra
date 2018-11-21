4246c6726eba0bf4daa3b291bd78cf7e236c4207
Unknown <lacknersmichael@gmail.com>
2018-11-21 21:46:29 +0100

    deleted Junk in tests

--- test/runtests.jl
+++
@@ -1,9 +0,0 @@
-using Revise, Pkg
-
-Pkg.activate("/Users/lackner/Documents/Projects/Northwestern/")
-
-using NUSFSpectra
-
-folder = "/Users/lackner/Documents/Projects/Northwestern/Michael/2018-11-15/"
-
-spectra = batchprocess(folder)
