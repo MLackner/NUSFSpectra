dd18ed09c55fb3a382ce93738b8bad380700324e
MLackner <lacknersmichael@gmail.com>
2018-11-23 19:10:43 +0100

    use integers

--- pythonmodules/pySfgProcess/spectrum.py
+++ pythonmodules/pySfgProcess/spectrum.py
@@ -506,7 +506,7 @@ class Spectrum():
         print('Padding DFGs with Zeros...')
         #dictionary to hold number of zeros to pad on either side
         padding = dict(det615=[0,467],det620=[58,409],det625=[116,351],det630=[174,293],
-                       det635=[232,235],det640=[290,177], det642=[303.8,153.4],det645=[349,118],
+                       det635=[232,235],det640=[290,177], det642=[304,153],det645=[349,118],
                        det655=[467,0])
 
         #length of fullwn is 911
@@ -535,7 +535,7 @@ class Spectrum():
         print('Padding ΒGs with Zeros...')
         #dictionary to hold number of zeros to pad on either side
         padding = dict(det615=[0,467],det620=[58,409],det625=[116,351],det630=[174,293],
-                       det635=[232,235],det640=[290,177], det642=[303.8,153.4],det645=[349,118],
+                       det635=[232,235],det640=[290,177], det642=[304,153],det645=[349,118],
                        det655=[467,0])
 
         #length of fullwn is 911
